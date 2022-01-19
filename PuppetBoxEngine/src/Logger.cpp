a46b4490b1677c18420690c3dac6da909f25795d
Atrum <krkidkyle@gmail.com>
2022-01-18 20:50:59 -0500

    Logger upgrade - appenders

---
+++ PuppetBoxEngine/src/Logger.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+
+#include "Logger.h"
+
+namespace PB
+{
+    class DefaultLoggingAppender : public LoggingAppender
+    {
+    public:
+        void send(const std::string& message)
+        {
+            std::cout << message << std::endl;
+        }
+    };
+
+    namespace DefaultLogger
+    {
+        static std::unique_ptr<LoggingAppender> appender_ = std::make_unique<DefaultLoggingAppender>();
+
+        static std::string getFileName(const std::string& filePath)
+        {
+            std::string fileName;
+
+            if (filePath.length() <= UINT32_MAX)
+            {
+                // If the first character of the string is \ then I don't care to remove it
+                for (std::size_t i = filePath.length() - 1; i > 0; --i)
+                {
+                    if (filePath.at(i) == '\\')
+                    {
+                        fileName = filePath.substr(static_cast<std::size_t>(i) + 1, filePath.length() - i + 1);
+                        i = 1; // Found it, we're done.
+                    }
+                }
+            }
+            else
+            {
+                fileName = "";
+                warn("File name was too long to parse", "Logger", 0);
+            }
+
+            return fileName;
+        };
+
+        static void log(
+                const std::string& level,
+                const std::string& message,
+                const std::string& filePath,
+                std::uint32_t line)
+        {
+            time_t now;
+            time(&now);
+            const std::string fileName = getFileName(filePath);
+            appender_->send(std::to_string(now) + " | " + level + " | " + fileName + " (" + std::to_string(line) + "): " + message);
+        };
+
+        void debug(const std::string& message, const std::string& filePath, std::uint32_t line)
+        {
+            log("DEBUG", message, filePath, line);
+        }
+
+        void warn(const std::string& message, const std::string& filePath, std::uint32_t line)
+        {
+            log("WARN", message, filePath, line);
+        }
+
+        void error(const std::string& message, const std::string& filePath, std::uint32_t line)
+        {
+            log("ERROR", message, filePath, line);
+        }
+
+        void setAppender(std::unique_ptr<LoggingAppender> loggingAppender)
+        {
+            appender_ = std::move(loggingAppender);
+        }
+    }
+}
--- PuppetBoxEngine/src/Logger.h
+++ PuppetBoxEngine/src/Logger.h
@@ -1,8 +1,7 @@
 #pragma once
 
-#include <iostream>
 #include <string>
-#include <utility>
+#include <memory>
 
 #define LOGGER_DEBUG(a) DefaultLogger::debug(a,__FILE__,__LINE__)
 #define LOGGER_WARN(a) DefaultLogger::warn(a,__FILE__,__LINE__)
@@ -11,62 +10,28 @@
 namespace PB
 {
     /**
-    * \brief Logging class for application logging needs.
-    */
-    class DefaultLogger
+     * \brief Interface for defining an appender.
+     *
+     * <p>Appenders offer a means to modify logging behavior, such as defining alternative
+     * output targets.</p>
+     */
+    class LoggingAppender
     {
     public:
-        static void debug(const std::string& message, const std::string& filePath, std::uint32_t line)
-        {
-            log("DEBUG", message, filePath, line);
-        }
+        virtual void send(const std::string& message) = 0;
+    };
 
-        static void warn(const std::string& message, const std::string& filePath, std::uint32_t line)
-        {
-            log("WARN", message, filePath, line);
-        }
+    /**
+    * \brief Logging class for application logging needs.
+    */
+    namespace DefaultLogger
+    {
+        void debug(const std::string& message, const std::string& filePath, std::uint32_t line);
 
-        static void error(const std::string& message, const std::string& filePath, std::uint32_t line)
-        {
-            log("ERROR", message, filePath, line);
-        }
+        void warn(const std::string& message, const std::string& filePath, std::uint32_t line);
 
-    private:
-        static void log(
-                const std::string& level,
-                const std::string& message,
-                const std::string& filePath,
-                std::uint32_t line)
-        {
-            time_t now;
-            time(&now);
-            const std::string fileName = getFileName(filePath);
-            std::cout << now << " | " << level << " | " << fileName << " (" << line << "): " << message << std::endl;
-        };
+        void error(const std::string& message, const std::string& filePath, std::uint32_t line);
 
-        static std::string getFileName(const std::string& filePath)
-        {
-            std::string fileName;
-
-            if (filePath.length() <= UINT32_MAX)
-            {
-                // If the first character of the string is \ then I don't care to remove it
-                for (std::size_t i = filePath.length() - 1; i > 0; --i)
-                {
-                    if (filePath.at(i) == '\\')
-                    {
-                        fileName = filePath.substr(static_cast<std::size_t>(i) + 1, filePath.length() - i + 1);
-                        i = 1; // Found it, we're done.
-                    }
-                }
-            }
-            else
-            {
-                fileName = "";
-                LOGGER_WARN("File name was too long to parse");
-            }
-
-            return fileName;
-        };
+        void setAppender(std::unique_ptr<LoggingAppender> loggingAppender);
     };
 }
